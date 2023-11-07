7a6f063c382f62c6824428820e94c11a62eb18b5
Leonardo Fernandes Zappani <102327012+Leonardo-Zappani@users.noreply.github.com>
2023-11-07 20:17:21 -0300

    começando a criar lista de produtos

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -1,29 +1,35 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 
 const Home: FC = () => {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    // Busca novos produtos dentro dessa API publica para listar 20 exemplos
+    // na tela principal.
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json => setProdutos(json))
+  }, [])
+
   return (
     <>
-      <section>
-        <div className="hero min-h-[calc(100vh-64px)] bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="/images/hero.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">Welcome</h1>
-              <p className="py-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-                minima laboriosam maxime sed dignissimos harum provident itaque
-                fugiat. A repellat aliquid inventore dolor tempora, omnis
-                perferendis aspernatur quo nisi excepturi. Ex, ullam odio iusto
-                esse necessitatibus doloremque repudiandae!
-              </p>
-              <button className="btn-primary btn">Get Started</button>
+        {produtos.map((produto) => (
+          <section>
+          <div key={produto.id} className="hero min-h-[calc(100vh-64px)] bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+              <img
+                src={produto.image}
+                className="max-w-sm rounded-lg shadow-2xl"
+                alt={produto.title}
+              />
+              <div>
+                <h1 className="text-5xl font-bold">{produto.title}</h1>
+                <button className="btn-primary btn">Comprar</button>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+          </section>
+        ))}
     </>
   );
 };
