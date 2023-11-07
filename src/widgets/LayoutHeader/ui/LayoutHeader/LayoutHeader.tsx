f06737067853351a0b98e4f9a3d0e17838e21001
Leonardo Fernandes Zappani <102327012+Leonardo-Zappani@users.noreply.github.com>
2023-11-07 20:17:01 -0300

    adicionando navbar com carrinho e popup da pessoa

--- src/widgets/LayoutHeader/ui/LayoutHeader/LayoutHeader.tsx
+++ src/widgets/LayoutHeader/ui/LayoutHeader/LayoutHeader.tsx
@@ -3,57 +3,50 @@ import { Logo } from "@/widgets";
 import Menu from "../Menu/Menu";
 
 const LayoutHeader: FC = () => {
+ 
   return (
     <>
-      <header>
-        <nav className="navbar bg-base-100">
-          <Menu
-            links={[
-              { name: "Homepage", href: "/" },
-              { name: "Portfolio", href: "/portfolio" },
-              { name: "About", href: "/about" },
-            ]}
-          />
-          <Logo logoName={"daisyUI"} />
-          <div className="navbar-end">
-            <button className="btn-ghost btn-circle btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-            <button className="btn-ghost btn-circle btn">
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">E-conomize</a>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
               <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-                <span className="badge badge-primary badge-xs indicator-item"></span>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="badge badge-sm indicator-item">8</span>
               </div>
-            </button>
+            </label>
+            <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Itens</span>
+                <span className="text-info">Subtotal: R$123</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">Ver carrinho :)</button>
+                </div>
+              </div>
+            </div>
           </div>
-        </nav>
-      </header>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://s2-g1.glbimg.com/KijX0QgEENLEh2xN2h6bEyRnt_4=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/9/e/EYKMBXR72tNdkUAgmZzQ/azul-caneta.jpg" />
+              </div>
+            </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
