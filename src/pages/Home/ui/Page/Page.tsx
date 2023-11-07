3df240bb8c3708c167496b84c8d89b7770e45832
Leonardo Fernandes Zappani <102327012+Leonardo-Zappani@users.noreply.github.com>
2023-11-07 20:20:35 -0300

    adicionando display grid para os produtos

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -4,33 +4,31 @@ const Home: FC = () => {
   const [produtos, setProdutos] = useState([]);
 
   useEffect(() => {
-    // Busca novos produtos dentro dessa API publica para listar 20 exemplos
+    // Busca novos produtos dentro dessa API pública para listar 20 exemplos
     // na tela principal.
     fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
+    .then(res => res.json())
     .then(json => setProdutos(json))
-  }, [])
+  }, []);
 
   return (
-    <>
-        {produtos.map((produto) => (
-          <section>
-          <div key={produto.id} className="hero min-h-[calc(100vh-64px)] bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-              <img
-                src={produto.image}
-                className="max-w-sm rounded-lg shadow-2xl"
-                alt={produto.title}
-              />
-              <div>
-                <h1 className="text-5xl font-bold">{produto.title}</h1>
-                <button className="btn-primary btn">Comprar</button>
-              </div>
-            </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mx-32 my-8">
+      {produtos.map((produto) => (
+        <div key={produto.id} className="rounded-lg shadow-2xl bg-base-300">
+          <img
+            src={produto.image}
+            className="w-full h-64 object-cover rounded-t-lg"
+            alt={produto.title}
+          />
+          <div className="p-4">
+            <h1 className="text-xl font-bold">{produto.title}</h1>
+            <button className="mt-2 w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-600">
+              Comprar
+            </button>
           </div>
-          </section>
-        ))}
-    </>
+        </div>
+      ))}
+    </div>
   );
 };
 
