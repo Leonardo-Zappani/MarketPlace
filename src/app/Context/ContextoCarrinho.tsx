52fa4a2b88d0ab4514809c7be1798ce350e799e3
Leonardo Fernandes Zappani <102327012+Leonardo-Zappani@users.noreply.github.com>
2023-11-22 21:26:33 -0300

    criando contexto pro carrinho

---
+++ src/app/Context/ContextoCarrinho.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useState } from "react";
+
+type CartItem = {
+  id: string;
+  price: number;
+  image: string;
+  title: string;
+};
+
+type CartContextType = {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+};
+
+const ContextoCarrinho = createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prevCart) => [...prevCart, item]);
+  };
+
+  return <ContextoCarrinho.Provider value={{ cart, addToCart }}>{children}</ContextoCarrinho.Provider>;
+};
+
+export const useCart = () => {
+  const context = useContext(ContextoCarrinho);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,14 +2,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "@/app/store";
+import { CartProvider } from "../app/Context/ContextoCarrinho.tsx"; // Import CartProvider
 
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <CartProvider> {/* Wrap the existing providers with CartProvider */}
+                <App />
+            </CartProvider>
+        </Provider>
+    </BrowserRouter>,
 );
